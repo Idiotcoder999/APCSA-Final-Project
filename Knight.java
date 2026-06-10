bc7abacf46187fa0922a927f23b60d93908c75ed
Idiotcoder999 <166742786+Idiotcoder999@users.noreply.github.com>
2026-06-10 11:56:21 -0400

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -20,5 +20,9 @@ public class Knight extends Character{
 	public void heal() {
 		//to be implemented
 	}
+	
+	public String toString() {
+		return "Knight Level: " + super.getLevel() + " Attack Damages: " + super.getlAtt() + " " + super.gethAtt();
+	}
 
 }
