fe4b20ea77024573268509207ca373bd5a8403b2
Idiotcoder999 <166742786+Idiotcoder999@users.noreply.github.com>
2026-06-10 11:56:04 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,15 +1,22 @@
 
 public class Player extends Character{
 	private int money;
-	
-	public Player(int l, int h, int light, int heavy, int mon) {
+	private String name;
+
+	public Player(int l, int h, int light, int heavy, int mon, String n) {
 		super(l, h, light, heavy);
+		startingHealth = h;
 		money = mon;
+		name = n;
 	}
-	
+
 	@Override
 	public void heal() {
-		//to be implemented
+		if(super.getHealth() >= startingHealth/2) {
+			if(money >= 50) {
+				super.setHealth(startingHealth); 
+			}
+		}
 	}
 
 	@Override
@@ -20,5 +27,44 @@ public class Player extends Character{
 	@Override
 	public void heavyAttack(Character c) {
 		//to be implemented
+	}
+	
+	public String toString() {
+		return "Name: " + name + " Player Level: " + super.getLevel() + " Attack Damages: " + super.getlAtt() + " " + super.gethAtt();
+	}
+}
+
+public class Player extends Character{
+	private int money;
+	private String name;
+
+	public Player(int l, int h, int light, int heavy, int mon, String n) {
+		super(l, h, light, heavy);
+		startingHealth = h;
+		money = mon;
+		name = n;
+	}
+
+	@Override
+	public void heal() {
+		if(super.getHealth() >= startingHealth/2) {
+			if(money >= 50) {
+				super.setHealth(startingHealth); 
+			}
+		}
+	}
+
+	@Override
+	public void lightAttack(Character c) {
+		//to be implemented
+	}
+
+	@Override
+	public void heavyAttack(Character c) {
+		//to be implemented
+	}
+	
+	public String toString() {
+		return "Name: " + name + " Player Level: " + super.getLevel() + " Attack Damages: " + super.getlAtt() + " " + super.gethAtt();
 	}
 }
