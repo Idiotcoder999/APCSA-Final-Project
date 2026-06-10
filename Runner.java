498a6c550d495558c3e828f3c684dfc3d9b22548
Idiotcoder999 <166742786+Idiotcoder999@users.noreply.github.com>
2026-06-10 11:55:32 -0400

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -1,7 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Runner {
 
 	public static void main(String[] args) {
-		//to be implemented
+		ArrayList<Character> enemies = new ArrayList<Character>();
+		enemies = setUp();
+		System.out.println(enemies);
+	}
+	
+	public static ArrayList<Character> setUp() {
+		Scanner scanKey = new Scanner(System.in);
+		System.out.print("How many enemies would you like?: ");
+		int n = scanKey.nextInt();
+		System.out.print("\nWhat is your name?: ");
+		String name = scanKey.next();
+		Game.populate(n);
+		Player p1 = new Player(1, 100, 2, 4, 100, name);
+		System.out.println(p1);
+		System.out.println();
+		return Game.populate(n);
 	}
 
 }
