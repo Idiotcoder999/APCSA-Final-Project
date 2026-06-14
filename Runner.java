38d9ce8000deb38a324d7f82c53c6dcecac6b809
Idiotcoder999 <166742786+Idiotcoder999@users.noreply.github.com>
2026-06-14 11:32:22 -0400

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -6,7 +6,7 @@ public class Runner {
 	public static void main(String[] args) {
 		ArrayList<Character> enemies = new ArrayList<Character>();
 		enemies = setUp();
-		System.out.print("\nWhat is your name?: ");
+		System.out.print("What is your name?: ");
 		Scanner scanKey = new Scanner(System.in);
 		String name = scanKey.next();
 		Player p = new Player(1, 100, 10, 15, 100, name);
@@ -54,40 +54,122 @@ public class Runner {
 				int rand = (int) (Math.random() * (lvl1.size()));
 				enemy = lvl1.get(rand);
 				enemyHealth = enemy.getHealth();
-				System.out.println("Your enemy is a " + enemy);
+				System.out.println("\nYour enemy is a " + enemy + "\n");
 				while(!enemy.isDead() && !p.isDead()) {
-					int dodgeChance = (int) (Math.random() * 99) + 1;
+					System.out.println("Your health is " + p.getHealth());
+					System.out.println("The enemy's health is " + enemy.getHealth());
+					System.out.println("Would you like to heal? Enter Yes or No");
+					String heal = scanKey.next().toLowerCase();
+					boolean HealTrue = heal.equals("yes");
+					int dodgeChance = (int) (Math.random() * 10) + 1;
 					int attChance = (int) (Math.random() * 3) + 1;
-					System.out.print("1 for a light attack, 2 for a heavy attack: ");
+					if(!HealTrue) {
+					System.out.print("\n1 for a light attack, 2 for a heavy attack: ");
 					int att = scanKey.nextInt();
 					if(att == 1) 
 						p.lightAttack(enemy);
 					if(att == 2) {
 						p.heavyAttack(enemy);
 					}
-					System.out.println("Enemies health is now: " + enemy.getHealth());
+					} else {
+						p.heal();
+					}
+					System.out.println("\nEnemies health is now: " + enemy.getHealth());
+					if(dodgeChance != 1) {
+						if(attChance == 1)
+							enemy.heavyAttack(p);
+						else 
+							enemy.heavyAttack(p);
+					} else
+						System.out.println("\nYou dodged the enemy's attack");
 				}
 				if(enemy.isDead()) {
+					System.out.println("\nThe enemy is dead!");
 					p.increaseEXP(enemyHealth);
 					p.levelUp();
+					((Player) p).incMoney(enemyHealth);
 				}
 			}
 			
 			if(p.getLevel() == 2) {
-				int rand = (int) (Math.random() * (lvl2.size() - 1));
+				int rand = (int) (Math.random() * (lvl2.size()));
 				enemy = lvl2.get(rand);
-				System.out.println("Your enemy is a " + enemy);
-				while(!enemy.isDead()) {
-					
+				enemyHealth = enemy.getHealth();
+				System.out.println("\nYour enemy is a " + enemy + "\n");
+				while(!enemy.isDead() && !p.isDead()) {
+					System.out.println("Your health is " + p.getHealth());
+					System.out.println("The enemy's health is " + enemy.getHealth());
+					System.out.println("Would you like to heal? Enter Yes or No");
+					String heal = scanKey.next().toLowerCase();
+					boolean HealTrue = heal.equals("yes");
+					int dodgeChance = (int) (Math.random() * 10) + 1;
+					int attChance = (int) (Math.random() * 3) + 1;
+					if(!HealTrue) {
+					System.out.print("\n1 for a light attack, 2 for a heavy attack: ");
+					int att = scanKey.nextInt();
+					if(att == 1) 
+						p.lightAttack(enemy);
+					if(att == 2) {
+						p.heavyAttack(enemy);
+					}
+					} else {
+						p.heal();
+					}
+					System.out.println("\nEnemies health is now: " + enemy.getHealth());
+					if(dodgeChance != 1) {
+						if(attChance == 1)
+							enemy.heavyAttack(p);
+						else 
+							enemy.heavyAttack(p);
+					} else
+						System.out.println("\nYou dodged the enemy's attack");
+				}
+				if(enemy.isDead()) {
+					System.out.println("\nThe enemy is dead!");
+					p.increaseEXP(enemyHealth);
+					p.levelUp();
+					((Player) p).incMoney(enemyHealth);
 				}
 			}
 			
 			if(p.getLevel() == 3) {
-				int rand = (int) (Math.random() * (lvl3.size() - 1));
+				int rand = (int) (Math.random() * (lvl3.size()));
 				enemy = lvl3.get(rand);
-				System.out.println("Your enemy is a " + enemy);
-				while(!enemy.isDead()) {
-					
+				enemyHealth = enemy.getHealth();
+				System.out.println("\nYour enemy is a " + enemy + "\n");
+				while(!enemy.isDead() && !p.isDead()) {
+					System.out.println("Your health is " + p.getHealth());
+					System.out.println("The enemy's health is " + enemy.getHealth());
+					System.out.println("Would you like to heal? Enter Yes or No");
+					String heal = scanKey.next().toLowerCase();
+					boolean HealTrue = heal.equals("yes");
+					int dodgeChance = (int) (Math.random() * 10) + 1;
+					int attChance = (int) (Math.random() * 3) + 1;
+					if(!HealTrue) {
+					System.out.print("\n1 for a light attack, 2 for a heavy attack: ");
+					int att = scanKey.nextInt();
+					if(att == 1) 
+						p.lightAttack(enemy);
+					if(att == 2) {
+						p.heavyAttack(enemy);
+					}
+					} else {
+						p.heal();
+					}
+					System.out.println("\nEnemies health is now: " + enemy.getHealth());
+					if(dodgeChance != 1) {
+						if(attChance == 1)
+							enemy.heavyAttack(p);
+						else 
+							enemy.heavyAttack(p);
+					} else
+						System.out.println("\nYou dodged the enemy's attack");
+				}
+				if(enemy.isDead()) {
+					System.out.println("\nThe enemy is dead!");
+					p.increaseEXP(enemyHealth);
+					p.levelUp();
+					((Player) p).incMoney(enemyHealth);
 				}
 			}
 		}
